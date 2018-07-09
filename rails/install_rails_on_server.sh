c21c28e06313ff838f388be334615f8c29682127
Uy Binh <uybinh@gmail.com>
2018-07-09 16:28:25 +0700

    Add other way to install postgres

--- rails/install_rails_on_server.sh
+++ rails/install_rails_on_server.sh
@@ -12,12 +12,15 @@ sudo apt-get install curl git-core nginx -y
 
 # 2. Installing Databases
 # Postgresql
+# Guide on unbuntu 18.04
+# sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+# wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install postgresql-common -y
+# sudo apt-get install postgresql-9.5 libpq-dev -y
 
-sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install postgresql-common -y
-sudo apt-get install postgresql-9.5 libpq-dev -y
+# Guide on DigitalOcean
+sudo apt-get install postgresql postgresql-contrib libpq-dev
 
 # create user
 # sudo -u postgres createuser -s pgbinh
@@ -28,7 +31,6 @@ sudo apt-get install postgresql-9.5 libpq-dev -y
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
 sudo apt-get update
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn -y
 
