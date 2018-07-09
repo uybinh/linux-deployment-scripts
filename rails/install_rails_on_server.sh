458ddddb385f0453335783b527914f8275f62fe6
Uy Binh <uybinh@gmail.com>
2018-07-09 17:56:02 +0700

    Add sudo to gpg

--- rails/install_rails_on_server.sh
+++ rails/install_rails_on_server.sh
@@ -36,7 +36,7 @@ sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libread
 
 # 4. Install rvm
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install 2.5.1
