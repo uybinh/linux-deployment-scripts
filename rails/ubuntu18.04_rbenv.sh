7b894e5cc6d5b7e317b74aeffcd9eda7a2fb9df6
Uy Binh <uybinh@gmail.com>
2018-07-10 08:00:26 +0700

    Add script to install rails with rbenv

---
+++ rails/ubuntu18.04_rbenv.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# 1. Install rbenv
+# 1.1 Before install
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+# 1.2 Install rbenv
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+# 1.3 Install ruby-build as a rbenv plugin
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+source .bashrc
+
+rbenv install 2.5.1
+rbenv global 2.5.1
+ruby -v
+
+# Rubygems not generate local documentation for each gem that you install
+echo "gem: --no-document" > ~/.gemrc
+gem install bundler
+rbenv rehash
+
+# 2. Install Rails
+# 2.1 install NodeJS
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# 2.2 intall Rails
+gem install rails -v 5.2.0
+rbenv rehash
+
+# 3. Install Postgres
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-common
+sudo apt-get install postgresql-9.5 libpq-dev
