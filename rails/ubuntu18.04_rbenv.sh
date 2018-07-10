045612d102b6513010cd2f5573b32591cfd7caea
Uy Binh <uybinh@gmail.com>
2018-07-10 09:49:28 +0700

    Don't use 'source'

--- rails/ubuntu18.04_rbenv.sh
+++ rails/ubuntu18.04_rbenv.sh
@@ -9,19 +9,22 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
-# 1.2 Install rbenv
+# 1.2 rbenv
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+grep -q 'export PATH="$HOME/.rbenv/bin:$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+grep -q 'eval "$(rbenv init -)"' ~/.bashrc || echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-source ~/.bashrc
 
-# 1.3 Install ruby-build as a rbenv plugin
+
+# 1.3 ruby-build as a rbenv plugin
 mkdir -p "$(rbenv root)"/plugins
 git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+grep -q 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+
+~/.rbenv/bin/rbenv init
 
 rbenv install 2.5.1
 rbenv global 2.5.1
@@ -34,17 +37,16 @@ rbenv rehash
 # Note: rbenv doctor script
 #curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
-
 # 2. Install Rails
-# 2.1 install NodeJS
+# 2.1 NodeJS
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# 2.2 intall Rails
+# 2.2 Rails
 gem install rails -v 5.2.0
 rbenv rehash
 
-# 3. Install Postgres
+# 3. Postgres
 sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
