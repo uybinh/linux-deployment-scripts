968f80827f47bcc931a8b8c46fca0fe449be9ff4
Uy Binh <uybinh@gmail.com>
2018-07-10 08:06:19 +0700

    Set default y for prompt

--- rails/ubuntu18.04_rbenv.sh
+++ rails/ubuntu18.04_rbenv.sh
@@ -7,7 +7,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
 # 1.2 Install rbenv
 cd
@@ -44,5 +44,5 @@ rbenv rehash
 sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-common
-sudo apt-get install postgresql-9.5 libpq-dev
+sudo apt-get install -y postgresql-common
+sudo apt-get install -y postgresql-9.5 libpq-dev
